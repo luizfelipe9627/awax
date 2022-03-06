f4c31aeb8c2821d14459e66513572f01aa07c9b3
luizfelipe9627 <felipesilva9627@hotmail.com>
2022-03-06 16:25:47 -0300

    update

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    document.querySelector(".menu_mobile"). addEventListener("click", function () {
+        if(document.querySelector(".menu nav").style.display == 'flex') {
+            document.querySelector(".menu nav").style.display = 'none';
+        }   
+        
+        else {
+            document.querySelector(".menu nav").style.display = 'flex';
+        }
+    });
+};
